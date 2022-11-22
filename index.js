6299d42e3ca2b6da3fdec3936537a2867a9170b6
alejandroballesta <alejandroballesta@facephi.com>
2022-11-22 16:41:46 +0100

    finished ejercicio 01

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const path = require('path');
+const { auth } = require('express-oauth2-jwt-bearer');
 
 const app = express()
 const port = 3000
+
+const checkJwt = auth({
+  audience: 'https://api-js.com/pruebas',
+  issuerBaseURL: 'https://dev-mjmzgyd7.us.auth0.com'
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './web/index.html'));
 })
 
-app.get('/api/protectedData', (req, res) => {
+app.get('/api/protectedData', checkJwt, (req, res) => {
   res.send('this is important data')
 })
 
